55ed15f1b9250131c51dd98d75f796049858cd7a
ian-kerins <60484445+ian-kerins@users.noreply.github.com>
2022-02-02 06:40:53 +0100

    Update settings.py

--- instascraper/settings.py
+++ instascraper/settings.py
@@ -14,6 +14,19 @@ BOT_NAME = 'instascraper'
 SPIDER_MODULES = ['instascraper.spiders']
 NEWSPIDER_MODULE = 'instascraper.spiders'
 
+## Add Your ScrapeOps API key
+SCRAPEOPS_API_KEY = 'YOUR_API_KEY' ## get free API key at https://scrapeops.io/app/register
+
+## Add In The ScrapeOps Extension
+EXTENSIONS = {
+ 'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+}
+
+## Update The Download Middlewares
+DOWNLOADER_MIDDLEWARES = { 
+'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550, 
+'scrapy.downloadermiddlewares.retry.RetryMiddleware': None, 
+}
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'instascraper (+http://www.yourdomain.com)'
